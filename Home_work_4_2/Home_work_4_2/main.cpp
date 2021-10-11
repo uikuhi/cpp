556bae17bbcba45026848e3b3c6f63fce90a2265
Misha Senikov <misha.senikov132@gmail.com>
2021-10-11 11:33:20 +0300

    unical numbers

---
+++ Home_work_4_2/Home_work_4_2/main.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main()
+{
+
+    srand(time(nullptr));
+
+    constexpr size_t size_a = 30;
+    int A[size_a] = {};
+    for(size_t i = 0; i < size_a; ++i){
+        A[i] = rand() % 10 + 5;
+        printf("%u) %d  ", i , A[i]);
+    }
+    printf("\n");
+
+    srand(time(nullptr));
+
+    constexpr size_t size_b = 15;
+    int B[size_b] = {};
+    for(size_t i = 0; i < size_b; ++i){
+        B[i] = rand() % 10;
+        printf("%u) %d  ", i , B[i]);
+    }
+    printf("\n");
+
+    printf("Result index: ");
+    for(size_t i = 0; i < size_a; ++i){
+
+        bool found = false;
+
+        for(size_t j = 0; !found && j < size_b; ++j){
+            if(A[i] == B[j]){
+                found = true;
+            }
+        }
+
+        if(!found){
+            printf("%u ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
